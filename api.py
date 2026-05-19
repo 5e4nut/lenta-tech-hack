423da617141ef3e57748e5111041928df576c857
5e4nut <vavavan200313@gmail.com>
2026-05-19 05:05:51 +0300

    закинул фронт, добавил запуск с батника

--- api.py
+++ api.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
 from pathlib import Path
 import shutil
 import subprocess
@@ -7,12 +10,25 @@ import uuid
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_methods=["POST"],
+    allow_headers=["*"],
+)
+
 UPLOAD_DIR = Path("uploads")
 RESULT_DIR = Path("results")
 
 UPLOAD_DIR.mkdir(exist_ok=True)
 RESULT_DIR.mkdir(exist_ok=True)
 
+app.mount("/static", StaticFiles(directory="front/static"), name="static")
+
+@app.get("/", response_class=HTMLResponse)
+async def serve_frontend():
+    index = Path("front/index.html")
+    return index.read_text()
 
 @app.post("/process-video")
 async def process_video(file: UploadFile = File(...)):
@@ -50,3 +66,7 @@ async def process_video(file: UploadFile = File(...)):
         filename="result.csv",
         media_type="text/csv"
     )
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
